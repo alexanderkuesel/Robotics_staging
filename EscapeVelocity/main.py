f32c0b2d3af5de9e059d7d0fc940d125bd299ad2
alexanderkuesel <107973606+alexanderkuesel@users.noreply.github.com>
2024-06-05 15:56:43 -0600

    test user input in settings.json

--- EscapeVelocity/main.py
+++ EscapeVelocity/main.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 from OrbitExtractor import targetOrbit
 
-# Load the JSON configuration file
+# Load json config
 current_dir = os.path.dirname(os.path.abspath(__file__))
 zip_dir = os.path.join(current_dir, '/zips')
 config_path = os.path.join(current_dir, 'config/settings.json')
@@ -15,13 +15,18 @@ OC_A = targetOrbit(**config['OC_A'])
 OC_B = targetOrbit(**config['OC_B'])
 OC_SCOUT = targetOrbit(**config['OC_SCOUT'])
 
-##User input space
-mission_name = 'OC23 - BCD Levels' ##make sure it is the EXACT name, including any accidental spaces
+
+##!!!!User input space!!!!!
+#mission_name = 'OC23 - BCD Levels' ##make sure it is the EXACT name, including any accidental spaces
+mission_name = config['User_input']
 start_time = '2024-05-27T12:00:00.00' ##needs to be in isostring format
 end_time = datetime.now() ##default is to pull everything up to NOW
 hourly_step = 6 ##only change if your results are seemingly limited by the 20 run id call limit in Orbit. 
 
+
 ##Execution Space
-print()
+print(f'Gathering all zip files for: {mission_name} in range {start_time} to {end_time}')
 OC_B.crawler(mission_name, start_time)
+
+print(f'Extracting jpgs from {current_dir}')
 OC_B.jpg_extract(current_dir, 'jpgOutput')
