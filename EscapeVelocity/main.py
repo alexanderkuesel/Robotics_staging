686c2a8c85f294adc24805cb586de7ea2d006a3e
alexanderkuesel <107973606+alexanderkuesel@users.noreply.github.com>
2024-06-05 16:32:33 -0600

    added element access to .json

--- EscapeVelocity/main.py
+++ EscapeVelocity/main.py
@@ -18,7 +18,7 @@ OC_SCOUT = targetOrbit(**config['OC_SCOUT'])
 
 ##!!!!User input space!!!!!
 #mission_name = 'OC23 - BCD Levels' ##make sure it is the EXACT name, including any accidental spaces
-mission_name = config['User_input']
+mission_name = config['User_input']['mission_name']
 start_time = '2024-05-27T12:00:00.00' ##needs to be in isostring format
 end_time = datetime.now() ##default is to pull everything up to NOW
 hourly_step = 6 ##only change if your results are seemingly limited by the 20 run id call limit in Orbit. 
