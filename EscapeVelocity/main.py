710301062e2b433a5b251c72cd52ccc79735edb0
alexanderkuesel <alexander.kuesel.montealegre@intel.com>
2024-06-12 12:48:38 -0600

    removed the hard-coded API tokens and added requirement to have a keys.json file with users specific tokens

--- EscapeVelocity/main.py
+++ EscapeVelocity/main.py
@@ -12,15 +12,18 @@ config_path = os.path.join(current_dir, 'config/settings.json')
 with open(config_path, 'r') as config_file:
     config = json.load(config_file)
 
+key_path = os.path.join(current_dir, 'config/keys.json')
+with open(key_path, 'r') as key_file:
+    keys = json.load(key_file)
+
 ##user changeable settings (mission name, etc)
 pref_path = os.path.join(current_dir, 'config/preferences.json')
 with open(pref_path, 'r') as pref_file:
     pref = json.load(pref_file)
 
-## Instance creation with each server (using settings.json)
-OC_A = targetOrbit(**config['OC_A'])
-OC_B = targetOrbit(**config['OC_B'])
-OC_SCOUT = targetOrbit(**config['OC_SCOUT'])
+## Instance creation with each server (using settings.json and last arg from keys.json
+OC_A = targetOrbit(config['OC_A']['server_name'], config['OC_A']['description'], config['OC_A']['robot_list'], config['OC_A']['url'], config['OC_A']['campus'], keys['OC_A']['token'])
+OC_B = targetOrbit(config['OC_B']['server_name'], config['OC_B']['description'], config['OC_B']['robot_list'], config['OC_B']['url'], config['OC_B']['campus'], keys['OC_B']['token'])
 
 ##Get user preferences from json
 target_server = pref['User_input']['target_server']
@@ -42,9 +45,4 @@ match target_server:
         OC_B.crawler(mission_name, start_time, datetime.now(), 24)
         print(f'Extracting jpgs from {current_dir}')
         OC_B.jpg_extract(current_dir, filter,'jpgOutput')
-    case 'OC_SCOUT':
-        print(f'Gathering all zip files from {target_server} for: {mission_name} in range {start_time} to {end_time}')
-        OC_SCOUT.crawler(mission_name, start_time, datetime.now(), 24)
-        print(f'Extracting jpgs from {current_dir}')
-        OC_SCOUT.jpg_extract(current_dir, filter,'jpgOutput')
 
